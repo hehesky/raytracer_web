e1894a6de4c09016cc71f022179f65873e4ab94f
hehesky <lk.genesis@163.com>
2017-11-24 19:56:33 -0500

    lambda

--- app/lambda_lib.py
+++ app/lambda_lib.py
@@ -19,18 +19,3 @@ def invoke_render(request_dict):
     """Assumes request_dict is a dictionary contains all info"""
     payload=json.dumps(request_dict).encode("ascii")#binary payload
     client.invoke(FunctionName=FUNCTION_NAME,InvocationType='Event',Payload=payload)
-
-if __name__=='__main__':
-    import uuid
-    reqid=str(uuid.uuid4())
-
-    d={
-        'id':reqid+'.png',
-        "entities":[
-            {"type":"sphere","center":'2.5,2.5,0','radius':'2','color':'1,0,0.1','reflectivity':'0.5'},
-            {"type":"sphere","center":'-2.5,2.5,0','radius':'2','color':'0,0,1','reflectivity':'0.5'},
-            {"type":"sphere","center":'-2.5,-2.5,0','radius':'2','color':'0,1,0.1','reflectivity':'0.5'},
-            {'position':'0,0,15','type':'light'}
-        ]
-    }
-    invoke_render(d)
